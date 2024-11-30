ebf62b79bf732c8223e8729d778ab994a4e1fe26
R.D.Sakamoto <skmt3p@gmail.com>
2024-11-30 23:29:42 +0900

    restore files

--- layers/base/app/utils/sleep.ts
+++ layers/base/app/utils/sleep.ts
@@ -5,9 +5,21 @@ import { nextTick } from 'vue'
  * @param { number } ms
  */
 export const sleep = (ms: number) =>
-  new Promise<void>(resolve => setTimeout(() => resolve(), ms))
+  new Promise<void>((resolve) => setTimeout(() => resolve(), ms))
 
+// NOTE: 参考: https://github.com/DevHIKKY/vketapp_front/pull/554/files#r1551786989
+// NOTE: どうしてこれで直っているのかは不明。だれかわかる人は参考URLのコメントで教えてください。そしてこのコメントを削除してください
+/**
+ * `await wrapper.get('input[type="text"]').setValue('12345678901')`
+ * などのアクションを待った時に、後続の`expect()`が失敗する場合に使う関数。
+ * ```ts
+ * await wrapper.get('input[type="text"]').setValue('12345678901')
+ * await waitEffect()
+ * expect(wrapper.get('p[class="error-container"]')).toBeTruthy()
+ * ```
+ * https://github.com/vuejs/vue-test-utils/issues/1406
+ */
 export const waitEffect = async () => {
   await nextTick()
-  await new Promise(resolve => requestAnimationFrame(resolve))
+  await new Promise((resolve) => requestAnimationFrame(resolve))
 }
