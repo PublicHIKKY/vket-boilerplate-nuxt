dd784bc13aa6adcfdf6f79d3808c0237c88f6e93
伊藤翔太/shota_ito <s.ito@vrhikky.com>
2024-12-27 12:18:47 +0900

    layerを追加

---
+++ layers/vket-sso/nuxt.config.ts
@@ -0,0 +1,45 @@
+import path from 'path'
+import { defineNuxtConfig } from 'nuxt/config'
+import { readEnvType } from './config/models/EnvType'
+import { getRuntimeConfigOfEnvType } from './config/runtimeConfig'
+
+const NUXT_ENV_OUTPUT_ENV = readEnvType(process.env)
+const runtimeConfig = getRuntimeConfigOfEnvType(
+  NUXT_ENV_OUTPUT_ENV,
+  process.env,
+)
+const srcDir = 'app'
+const isSsr = true
+const checkTypeCheckOnBuild = true
+const needAnalyze = NUXT_ENV_OUTPUT_ENV === 'local'
+const needSourcemap = NUXT_ENV_OUTPUT_ENV !== 'production'
+const enableDebug = NUXT_ENV_OUTPUT_ENV === 'local'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: path.resolve(__dirname, '../base'),
+
+  ssr: isSsr,
+  imports: {
+    dirs: ['utils/types/**'],
+    global: false,
+  },
+  runtimeConfig,
+  rootDir: __dirname,
+  srcDir: `${srcDir}/`,
+  alias: {
+    '#vket-sso': __dirname,
+  },
+  build: {
+    analyze: needAnalyze,
+  },
+  sourcemap: {
+    server: needSourcemap,
+    client: needSourcemap,
+  },
+  compatibilityDate: '2024-04-03',
+  typescript: {
+    typeCheck: checkTypeCheckOnBuild,
+  },
+  debug: enableDebug,
+})
