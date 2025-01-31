2e357f124f7fc8c8af913069521f961cd8a8bb68
kobito <u.zxdxc@icloud.com>
2025-01-31 10:05:57 +0900

    確認用の記述を削除

--- layers/main/app/app.vue
+++ layers/main/app/app.vue
@@ -29,92 +29,11 @@
     </template>
   </Head>
   <div class="app">
-    <HmSlider
-      class="news-slider"
-      slidename="slider1"
-      :items-id="['slide1-1', 'slide1-2', 'slide1-3']"
-      :arrow="true"
-      :pagination="true"
-      :amount="3"
-      :loop="true"
-      :center="false"
-      :page="true"
-      :autoplay="true"
-      :interval="7000"
-      gap-pc="0"
-      gap-sp="0"
-      width-pc="50"
-      width-sp="100"
-      :duration="500"
-      easing="ease-in-out"
-      :draggable="true"
-    >
-      <template #item>
-        <HmSliderItem
-          id="slider1-1"
-        >
-          1
-        </HmSliderItem>
-        <HmSliderItem
-          id="slider1-2"
-        >
-          2
-        </HmSliderItem>
-        <HmSliderItem
-          id="slider1-3"
-        >
-          3
-        </HmSliderItem>
-      </template>
-    </HmSlider>
-    <HmSlider
-      class="news-slider"
-      slidename="slider2"
-      :items-id="['slide2-1', 'slide2-2', 'slide2-3', 'slide2-4']"
-      :arrow="true"
-      :pagination="true"
-      :amount="4"
-      :loop="true"
-      :center="true"
-      :page="false"
-      :autoplay="true"
-      :interval="7000"
-      gap-pc="0"
-      gap-sp="0"
-      width-pc="100"
-      width-sp="100"
-      :duration="500"
-      easing="ease-in-out"
-      :draggable="true"
-    >
-      <template #item>
-        <HmSliderItem
-          id="slider2-1"
-        >
-          1
-        </HmSliderItem>
-        <HmSliderItem
-          id="slider2-2"
-        >
-          2
-        </HmSliderItem>
-        <HmSliderItem
-          id="slider2-3"
-        >
-          3
-        </HmSliderItem>
-        <HmSliderItem
-          id="slider2-4"
-        >
-          4
-        </HmSliderItem>
-      </template>
-    </HmSlider>
-    <!-- <NuxtLayout>
+    <NuxtLayout>
       <NuxtRouteAnnouncer />
       <NuxtWelcome />
       <NuxtPage />
-    </NuxtLayout> -->
+    </NuxtLayout>
   </div>
 </template>
 
